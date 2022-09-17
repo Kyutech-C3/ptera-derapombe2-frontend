71599864cf9c5f91b9b2e53ab3ea67b22efeb33c
citrus-candy <kyuuri11013@gmail.com>
2022-09-17 16:42:34 +0900

    add: linterの設定を追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+// eslint-disable-next-line import/no-unresolved
 import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
