fcf77795cbc3415ab97c63e7edd43e3908929860
citrus-candy <kyuuri11013@gmail.com>
2022-09-17 16:08:03 +0900

    react initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  root: './src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+  plugins: [react(), VitePWA()],
+})
