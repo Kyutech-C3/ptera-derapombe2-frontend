66748b7c6727c830bf961d44c65fa07d32ff11bb
citrus-candy <kyuuri11013@gmail.com>
2022-09-19 00:52:45 +0900

    change: マップの一部UIを非表示

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -89,6 +89,9 @@ function Map() {
   const mapOptions: google.maps.MapOptions = {
     minZoom: 12,
     maxZoom: 20,
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
   }
 
   return isLoaded ? (
