2cc0a4d79879beb6ac7aebd1677b61f092d4a66e
satou0515 <satsuki515.503.503@gmail.com>
2022-09-19 04:19:18 +0900

    背景設定、ボタン配置(途中経過)

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,5 +1,48 @@
+import React, { useState } from "react"
+import ReactDOM from "react-dom";
+import './inventory.css'
+
+
+
 function Inventory() {
-  return <p>inventory</p>
+    return (
+        <>
+            <body>
+                <header>ITEM</header>
+                <article className="subBack">
+                    <section className="setContents">
+                        <p>A</p>
+                        <p>B</p>
+                        <p>C</p>
+                    </section>
+                    <section className="setContents">
+                        <p>A</p>
+                        <p>B</p>
+                        <p>C</p>
+                    </section>
+                    <section className="setContents">
+                        <p>A</p>
+                        <p>B</p>
+                        <p>C</p>
+                    </section>
+                    <section className="setContents">
+                        <p>A</p>
+                        <p>B</p>
+                        <p>C</p>
+                    </section>
+                </article>
+                <footer>
+                    <div className="setHome">
+                        <button /*onClick={home}*/>HOME</button>
+                    </div>
+                    <div className="setBtn">
+                        <button /*onClick={items}*/>ITEM</button>
+                        <button /*onClick={collection}*/>COLLECT</button>
+                    </div>
+                </footer>
+            </body>
+        </>
+    );
 }
 
 export default Inventory
