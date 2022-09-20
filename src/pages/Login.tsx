7eb3e3cf2b358b5b50bea6ec9c735eb33ede8ba4
kyuuri11013 <kyuuri11013@gmail.com>
2022-09-20 20:56:06 +0900

    add: ログイン画面の作成

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,34 @@
 import { signInWithPopup, UserCredential } from 'firebase/auth'
 import { useState } from 'react'
+import styled from 'styled-components'
 import { auth, provider } from '../firebase'
+import { FcGoogle } from 'react-icons/fc'
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url('../assets/images/bg-login.png');
+  background-position: center;
+  background-size: cover;
+  background-color: #eaffe7;
+`
+
+const Icon = styled.img`
+  width: 100vw;
+  margin-top: 10vh;
+  margin-bottom: 20vh;
+`
+
+const LoginButton = styled.button`
+  display: flex;
+  align-items: center;
+`
+
+const Text = styled.span`
+  margin-left: 5px;
+`
 
 function Login() {
   const [user, setUser] = useState<UserCredential | null>(null)
@@ -16,13 +44,17 @@ function Login() {
   }
 
   return (
-    <>
-      {user ? <></> : <button onClick={handleLogin}>Google Login</button>}
-      <ul>
+    <Container>
+      <Icon src="../assets/images/icon-login.png" alt="icon-login" />
+      <LoginButton onClick={handleLogin}>
+        <FcGoogle size="25" />
+        <Text>Googleでログイン</Text>
+      </LoginButton>
+      {/* <ul>
         <li>displayName: {user?.user.displayName}</li>
         <li>uid: {user?.user.uid}</li>
-      </ul>
-    </>
+      </ul> */}
+    </Container>
   )
 }
 
